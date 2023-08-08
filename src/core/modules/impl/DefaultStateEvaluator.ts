74f2ca9361461c476350c171859a22929ed24fba
Elliot Sayes <elliot@sayes.cloud>
2023-08-08 20:30:03 +0800

    Also filter error messages

--- src/core/modules/impl/DefaultStateEvaluator.ts
+++ src/core/modules/impl/DefaultStateEvaluator.ts
@@ -317,10 +317,13 @@ export abstract class DefaultStateEvaluator implements StateEvaluator {
         // if that's an inner contract call - only update the state in the uncommitted states
         contract.interactionState().update(contract.txId(), new EvalStateResult(currentState, validity, errorMessages));
       }
+      const errorMessagesLocal: Record<string, string> = {
+        [missingInteraction.id]: errorMessages[missingInteraction.id]
+      };
       const validityLocal: Record<string, boolean> = {
         [missingInteraction.id]: validity[missingInteraction.id]
       };
-      const updateEvalStateResult = new EvalStateResult<State>(currentState, validityLocal, errorMessages);
+      const updateEvalStateResult = new EvalStateResult<State>(currentState, validityLocal, errorMessagesLocal);
       const updateSortKeyCacheResult = new SortKeyCacheResult(currentSortKey, updateEvalStateResult);
       stateUpdateHistory.push(updateSortKeyCacheResult);
     }
